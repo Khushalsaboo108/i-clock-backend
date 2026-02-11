ee40a6f61fd0dcc37e2b5a20c3166fc1eac28b6c
khushal saboo <khushalsaboo108@gmail.com>
2026-02-11 22:18:54 +0530

    update the domain name

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -60,7 +60,7 @@ export const login = asyncHandler(
         secure: process.env.NODE_ENV === "production" ? true : false,
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         path: "/",
-        domain: ".vercel.app",
+        domain: "localhost", // Change this to your domain in production
         maxAge: 7 * 24 * 60 * 60, // 7 days in seconds
       })
       .status(200)
